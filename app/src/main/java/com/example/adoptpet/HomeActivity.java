e0192f22399eb3d131ae76389a3d75d1f983c56b
nivvm <nivvm123@gmail.com>
2020-04-03 11:55:16 +0300

    add cardView.xml and first phase of rcyclerView

--- app/src/main/java/com/example/adoptpet/HomeActivity.java
+++ app/src/main/java/com/example/adoptpet/HomeActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class HomeActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -17,5 +20,54 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    }
+
+
+
+    private void fetch() {
+        Query query = FirebaseDatabase.getInstance()
+                .getReference()
+                .child("posts");
+
+        FirebaseRecyclerOptions<Model> options =
+                new FirebaseRecyclerOptions.Builder<Model>()
+                        .setQuery(query, new SnapshotParser<Model>() {
+                            @NonNull
+                            @Override
+                            public Model parseSnapshot(@NonNull DataSnapshot snapshot) {
+                                return new Model(snapshot.child("id").getValue().toString(),
+                                        snapshot.child("title").getValue().toString(),
+                                        snapshot.child("desc").getValue().toString());
+                            }
+                        })
+                        .build();
+
+        adapter = new FirestoreRecyclerAdapter<Pet, PetViewHolder>(options) {
+            @Override
+            public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.list_item, parent, false);
+
+                return new PetViewHolder(view);
+            }
+
+
+            @Override
+            protected void onBindViewHolder(PetViewHolder holder, final int position, Model model) {
+                //holder.setTxtTitle(model.getmTitle());
+                //holder.setTxtDesc(model.getmDesc());
+
+                holder.layout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(HomeActivity.this, String.valueOf(position), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+        };
+        recyclerView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/adoptpet/PetViewHolder.java
+++ app/src/main/java/com/example/adoptpet/PetViewHolder.java
@@ -1,31 +1,44 @@
 package com.example.adoptpet;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class PetViewHolder extends RecyclerView.ViewHolder {
-    public LinearLayout root;
-    public TextView txtTitle;
-    public TextView txtDesc;
+    public TextView txtName;
+    public TextView txtAge;
+    public TextView txtKind;
+    public TextView txtDogOrCat;
+    public ImageView imageView;
+    public LinearLayout layout;
+
 
     public PetViewHolder(@NonNull View itemView) {
         super(itemView);
-        //root = itemView.findViewById(R.id.list_root);
-        //txtTitle = itemView.findViewById(R.id.list_title);
-        //txtDesc = itemView.findViewById(R.id.list_desc);
+        txtName = itemView.findViewById(R.id.pet_name);
+        txtAge = itemView.findViewById(R.id.pet_age);
+        txtKind = itemView.findViewById(R.id.pet_kind);
+        txtDogOrCat = itemView.findViewById(R.id.pet_dog_or_cat);
+        imageView = itemView.findViewById(R.id.pet_image);
+        layout = itemView.findViewById(R.id.pet_layout);
     }
 
 
-    public void setTxtTitle(String string) {
-        txtTitle.setText(string);
+    public void setTxtName(String string) {
+        txtName.setText(string);
     }
-
-
-    public void setTxtDesc(String string) {
-        txtDesc.setText(string);
+    public void setTxtAge(String string) {
+        txtAge.setText(string);
+    }
+    public void setTxtKind(String string) {
+        txtKind.setText(string);
+    }
+    public void setTxtDogOrCat(String string) {
+        txtDogOrCat.setText(string);
     }
 }
