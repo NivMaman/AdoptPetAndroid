55d866cf99692f3b6ea71785ecbbcc0179f02c19
Niv Maman <niv.maman@harmonicinc.com>
2020-04-18 18:50:09 +0300

    finalize pet class, add cat/dog pictures defaults.

--- app/src/main/java/com/example/adoptpet/Pet.java
+++ app/src/main/java/com/example/adoptpet/Pet.java
@@ -1,17 +1,30 @@
 package com.example.adoptpet;
 
+import android.net.Uri;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.firestore.DocumentReference;
+
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Pet {
-    private String name;
-    private int age;
-    private String kind;
-    private String freeText;
-    private ArrayList<String> picturesUriArr;
+public class Pet implements Serializable {
+    private String  name; //Must
+    private long    age; //Must
+    private boolean dogOrCat; //Must true = cat, false = dog
+    private boolean isMale; //Must
+    private String  location;//Must
+    private String  phoneNumber;//Must
+    private String  contactName;//Must
+    private String  kind;// not a Must -- use set if needed
+    private String  freeText; // not a Must -- use set if needed
+
+    private ArrayList<String> picturesUriStringList;// not a Must -- use add if needed
+    private DocumentReference documentReference;
 
 
     // general definitions
@@ -20,43 +33,47 @@ public class Pet {
 
 
     //String definitions
-    private static final String AGE_STRING = "age";
-    private static final String KIND_STRING = "dogOrCat";
-    private static final String FREE_TEXT_STRING = "freeText";
-    private static final String NAME_STRING = "name";
-    private static final String PIC_ARRAY_STRING = "picturesList";
+    private static final String NAME_MAP_KEY = "name";
+    private static final String AGE_MAP_KEY = "age";
+    private static final String DOG_OR_CAT_MAP_KEY = "dogOrCat";
+    private static final String IS_MALE_MAP_KEY = "freeText";
+    private static final String LOCATION_MAP_KEY= "picturesList";
+    private static final String PHONE_NUMBER_MAP_KEY= "phoneNumber";
+    private static final String CONTACT_NAME_MAP_KEY= "contactName";
+    private static final String KIND_MAP_KEY= "kind";
+    private static final String FREE_TEXT_MAP_KEY = "freeText";
+    private static final String PIC_LIST_MAP_KEY= "picturesList";
 
 
-
-
-    public Pet(String name, int age, String kind, String FreeText, ArrayList <String> PicturesUriArr) {
+    public Pet(String name, long age, boolean dogOrCat, boolean isMale, String location, String phoneNumber, String contactName) {
         this.name = name;
         this.age = age;
-        this.kind = kind;
-        this.freeText = FreeText;
-        if(PicturesUriArr.size() == 3)
-            this.picturesUriArr = PicturesUriArr;
-        else {
-            this.picturesUriArr = new ArrayList<String>();
-            Log.e(TAG_LOG, "Pet() — PicturesUriArr.length != 3 ");
-            int i =0;
-            for(String path : picturesUriArr){
-                if (path!=null && i<3){
-                    this.picturesUriArr.add(path);
-                }
-                i++;
-            }
-        }
-        Log.d(TAG_LOG, "constructing a pet - " + this.toString());
+        this.dogOrCat = dogOrCat;
+        this.isMale = isMale;
+        this.location = location;
+        this.phoneNumber = phoneNumber;
+        this.contactName = contactName;
+
+        this.picturesUriStringList = new ArrayList<>();
+
+        Log.d(TAG_LOG, "constructing a pet -" + this.toString());
+
     }
+
 
     public Pet (Map<String,Object> map)
     {
-        this.age = (int) map.get(AGE_STRING);
-        this.kind = (String) map.get(KIND_STRING);
-        this.freeText = (String) map.get(FREE_TEXT_STRING);
-        this.name = (String) map.get(NAME_STRING);
-        this.picturesUriArr = ((ArrayList<String>)map.get(PIC_ARRAY_STRING));
+        this.name            = (String)  map.get(NAME_MAP_KEY);
+        this.age             = (long)    map.get(AGE_MAP_KEY);
+        this.dogOrCat        = (boolean) map.get(DOG_OR_CAT_MAP_KEY);
+        this.isMale          = (boolean) map.get(IS_MALE_MAP_KEY);
+        this.location        = (String)  map.get(LOCATION_MAP_KEY);
+        this.phoneNumber     = (String)  map.get(PHONE_NUMBER_MAP_KEY);
+        this.contactName     = (String)  map.get(CONTACT_NAME_MAP_KEY);
+        this.kind            = (String)  map.get(KIND_MAP_KEY);
+        this.freeText        = (String)  map.get(FREE_TEXT_MAP_KEY);
+        this.picturesUriStringList = ((ArrayList<String>)map.get(PIC_LIST_MAP_KEY));
+
         Log.d(TAG_LOG, "constructing a pet from map - " + this.toString());
 
     }
@@ -65,28 +82,128 @@ public class Pet {
     {
         Map<String,Object> map = new HashMap<>();
 
-        map.put(AGE_STRING,this.age);
-        map.put(KIND_STRING,this.kind);
-        map.put(FREE_TEXT_STRING,this.freeText);
-        map.put(NAME_STRING,this.age);
-        map.put(PIC_ARRAY_STRING,this.picturesUriArr);
+        map.put(NAME_MAP_KEY        ,this.name);
+        map.put(AGE_MAP_KEY         ,this.age);
+        map.put(DOG_OR_CAT_MAP_KEY  ,this.dogOrCat);
+        map.put(IS_MALE_MAP_KEY     ,this.isMale);
+        map.put(LOCATION_MAP_KEY    ,this.location);
+        map.put(PHONE_NUMBER_MAP_KEY,this.phoneNumber);
+        map.put(CONTACT_NAME_MAP_KEY,this.contactName);
+        map.put(KIND_MAP_KEY        ,this.kind);
+        map.put(FREE_TEXT_MAP_KEY   ,this.freeText);
+        map.put(PIC_LIST_MAP_KEY    ,this.picturesUriStringList);
 
         Log.d(TAG_LOG, "createMap - " + this.toString());
         return map;
     }
-/*
+
     @NonNull
     @Override
     public String toString() {
-        return "age = " + this.age + "dogOrCat = " + this.kind +
-        "freeText = " + this.freeText + "name = " + this.name + "picturesUriArr[0] = " + this.picturesUriArr[0] +
-                "picturesUriArr[1] = " + this.picturesUriArr[1] + "picturesUriArr[2] = " + this.picturesUriArr[2];
+        return " age = " + this.age + " dogOrCat = " + this.dogOrCat +
+                " isMale = " + this.isMale + " location = " + this.location +" phoneNumber = "+ this.phoneNumber + " contactName = " + this.contactName
+                + " freeText = " + this.freeText + " name = " + this.name +" picturesUriStringList = "+ this.picturesUriStringList + " freeText = " + this.freeText;
     }
 
- */
 
+
+    public Uri getMainPictureUri()
+    {
+        if(this.picturesUriStringList != null && this.picturesUriStringList.size()!= 0 )
+        {
+            return Uri.parse(picturesUriStringList.get(0));
+        }
+        else
+        {
+            //defaults pictures
+            int drawable;
+            if(dogOrCat)
+            {
+                //dog
+                drawable = R.drawable.dog_default;
+            }
+            else
+            {
+                //cat
+                drawable = R.drawable.cat_default;
+            }
+            return Uri.parse("android.resource://com.example.adoptpet/" + drawable);
+        }
+    }
+
+    public ArrayList<Uri> getPicturesUriArr()
+    {
+        ArrayList<Uri> picturesUriConverter = new ArrayList<>();
+        Uri converter;
+        for (String uri : this.picturesUriStringList)
+        {
+            converter = Uri.parse(uri);
+            picturesUriConverter.add(converter);
+        }
+        return picturesUriConverter;
+    }
+
+    public void addUriReference(String uri)
+    {
+        if (uri != null){
+            this.picturesUriStringList.add (uri);
+        }
+    }
+
+    public boolean isCat() {
+        return dogOrCat;
+    }
+    public boolean isDog() { return !dogOrCat; }
+
+    public DocumentReference getDocumentReference() {
+        return documentReference;
+    }
+
+    public void setDocumentReference(DocumentReference documentReference) {
+        this.documentReference = documentReference;
+    }
+
+    public boolean isDogOrCat() {
+        return dogOrCat;
+    }
+
+    public void setDogOrCat(boolean dogOrCat) {
+        this.dogOrCat = dogOrCat;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        this.isMale = male;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
     public String getFreeText() {
-        return freeText;
+        return this.freeText;
     }
 
     public void setFreeText(String freeText) {
@@ -94,43 +211,28 @@ public class Pet {
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public int getAge() {
-        return age;
+    public long getAge() {
+        return this.age;
     }
 
-    public void setAge(int age) {
+    public void setAge(long age) {
         this.age = age;
     }
 
     public String getKind() {
-        return kind;
+        return this.kind;
     }
 
     public void setKind(String kind) {
         this.kind = kind;
     }
-
-    public ArrayList<String> getPicturesUriArr() {
-        return this.picturesUriArr;
-    }
-
-    public void setPicturesUriArr(ArrayList<String> picturesUriArr) {
-        this.picturesUriArr = picturesUriArr;
-    }
-
-    public void addUriReference(String uri){
-        if (uri != null){
-            this.picturesUriArr.add (uri);
-        }
-    }
-
 }
 
 
