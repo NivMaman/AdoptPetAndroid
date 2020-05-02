ca94910d6a3e6c717b115d4807e75ba321fb5b7f
Niv Maman <niv.maman@harmonicinc.com>
2020-05-02 14:50:33 +0300

    added filters query, DBwraper functions.

--- app/src/main/java/com/example/adoptpet/AddPetActivity.java
+++ app/src/main/java/com/example/adoptpet/AddPetActivity.java
@@ -1,16 +1,22 @@
 package com.example.adoptpet;
 
+import com.example.adoptpet.R;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,80 +29,23 @@ import java.util.ArrayList;
 
 public class AddPetActivity extends AppCompatActivity {
 
-    private EditText fullName, age, freeText, contactNumber, contactName;
-    private BottomNavigationView bottomNavigationView;
-    private RadioGroup kindGroup, sexGroup;
-    private RadioButton kindButton, sexButton;
-    private FirebaseUser user;
-    private Button addToDb;
-    private ImageButton pic1;
-    private ArrayList<Uri> imagePathList;
-    private final int PICK_IMAGE_REQUEST = 71;
-    private static final String TAG = "AddPetActivity";
-    private String cities[] = {"Acre",
-            "Afula",
-            "Arad",
-            "Ariel",
-            "Ashdod",
-            "Ashkelon",
-            "Bat Yam",
-            "Beersheba",
-            "Beit Shean",
-            "Beit Shemesh",
-            "Beitar Illit",
-            "Bnei Brak",
-            "Dimona",
-            "Eilat",
-            "Elad",
-            "Givat Shmuel",
-            "Givatayim",
-            "Hadera",
-            "Haifa",
-            "Herzliya",
-            "Hod HaSharon",
-            "Holon",
-            "Jerusalem",
-            "Karmiel",
-            "Kiryat Ata",
-            "Kiryat Bialik",
-            "Kiryat Gat",
-            "Kiryat Malakhi",
-            "Kiryat Motzkin",
-            "Kiryat Ono",
-            "Kiryat Shmona",
-            "Kiryat Yam",
-            "Kfar Saba",
-            "Lod",
-            "Maale Adumim",
-            "Maalot-Tarshiha",
-            "Migdal HaEmek",
-            "Modiin-Maccabim-Reut",
-            "Modiin Illit",
-            "Nahariya",
-            "Nazareth Illit ",
-            "Nesher",
-            "Ness Ziona",
-            "Netanya",
-            "Netivot",
-            "Ofakim",
-            "Or Akiva",
-            "Or Yehuda",
-            "Petah Tikva",
-            "Raanana",
-            "Ramat Gan",
-            "Ramat HaSharon",
-            "Ramla",
-            "Rehovot",
-            "Rishon LeZion",
-            "Rosh HaAyin",
-            "Safed",
-            "Sderot",
-            "Tel Aviv-Yaffo",
-            "Tiberias",
-            "Tirat Carmel",
-            "Yavne",
-            "Yehud-Monosson",
-            "Yokneam"};
+    protected EditText fullNameEditText, ageEditText, freeTextEditText, contactNumberEditText, contactNameEditText
+            ,petKindEditText;
+    protected BottomNavigationView bottomNavigationView;
+    protected RadioGroup kindGroup, sexGroup;
+    protected RadioButton kindButton, sexButton;
+    protected FirebaseUser user;
+    protected Button addToDb;
+    protected ImageButton pic1;
+    protected ArrayList<Uri> imagePathList;
+    protected ImageView[] picturesView;
+    protected AutoCompleteTextView locationTextView;
+
+    protected final int PICK_IMAGE_REQUEST = 71;
+    protected static final String TAG = "AddPetActivity";
+
+    protected final static  int MAX_NUMBER_OF_IMAGES = 3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -125,17 +74,55 @@ public class AddPetActivity extends AppCompatActivity {
                 return false;
             }
         });
-        fullName = (EditText)findViewById(R.id.name);
-        age = (EditText)findViewById(R.id.age);
+        fullNameEditText = (EditText)findViewById(R.id.name);
+        ageEditText = (EditText)findViewById(R.id.age);
         kindGroup = (RadioGroup) findViewById(R.id.pet_kind);
         sexGroup = (RadioGroup) findViewById(R.id.pet_sex);
-        contactName = (EditText)findViewById(R.id.contact_name);
-        contactNumber = (EditText)findViewById(R.id.contact_number);
-        freeText = (EditText)findViewById(R.id.free_text);
+        contactNameEditText = (EditText)findViewById(R.id.contact_name);
+        contactNumberEditText = (EditText)findViewById(R.id.contact_number);
+        freeTextEditText = (EditText)findViewById(R.id.free_text);
         addToDb = (Button)findViewById(R.id.add_db);
         pic1 = (ImageButton) findViewById(R.id.add_pic);
+        picturesView = new ImageView[MAX_NUMBER_OF_IMAGES];
+        picturesView[0] = (ImageView) findViewById(R.id.pictureView1);
+        picturesView[1] = (ImageView) findViewById(R.id.pictureView2);
+        picturesView[2] = (ImageView) findViewById(R.id.pictureView3);
+        petKindEditText = (EditText) findViewById(R.id.pet_kind_edit_txt);
+
         pic1.setOnClickListener(getSelectPictureListener());
         addToDb.setOnClickListener(getAddToDbListener());
+        freeTextEditText.setOnKeyListener(getFreeTextOnKeyListener());
+
+
+        // Get a reference to the AutoCompleteTextView in the layout
+        locationTextView = (AutoCompleteTextView) findViewById(R.id.location);
+        // Get the string array
+        String[] cities = getResources().getStringArray(R.array.cities_array);
+        // Create the adapter and set it to the AutoCompleteTextView
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cities);
+        locationTextView.setAdapter(adapter);
+
+
+    }
+
+    private  View.OnKeyListener getFreeTextOnKeyListener(){
+        final int MAX_NUMBER_OF_LINES = 5;
+        return new View.OnKeyListener() {
+
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+
+
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+
+                    if (((EditText) v).getLineCount() >= MAX_NUMBER_OF_LINES)
+                        return true;
+                }
+
+                return false;
+            }
+        };
     }
 
     private View.OnClickListener getSelectPictureListener() {
@@ -149,24 +136,27 @@ public class AddPetActivity extends AppCompatActivity {
             }
         };
     }
-
+    protected Pet CollectPetInfo()
+    {
+        kindButton=(RadioButton)findViewById(kindGroup.getCheckedRadioButtonId());
+        sexButton=(RadioButton)findViewById(sexGroup.getCheckedRadioButtonId());
+        String name = fullNameEditText.getText().toString();
+        String sex = sexButton.getText().toString();
+        String dogOrCat = kindButton.getText().toString();
+        String number = contactNumberEditText.getText().toString();
+        String contact_name = contactNameEditText.getText().toString();
+        double age = Double.parseDouble(ageEditText.getText().toString());
+        String freeText = freeTextEditText.getText().toString();
+        String location = locationTextView.getText().toString();
+        String petKind  = petKindEditText.getText().toString();
+        return new Pet(name, age, dogOrCat, sex, petKind,freeText, location, number, contact_name);
+    }
     private View.OnClickListener getAddToDbListener() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                kindButton=(RadioButton)findViewById(kindGroup.getCheckedRadioButtonId());
-                sexButton=(RadioButton)findViewById(sexGroup.getCheckedRadioButtonId());
-                String name = fullName.getText().toString();
-                String sex = sexButton.getText().toString();
-                String kind = kindButton.getText().toString();
-                String number = contactNumber.getText().toString();
-                String contact_name = contactName.getText().toString();
-                int petAge = Integer.parseInt(age.getText().toString());
-                String description = freeText.getText().toString();
-                String location = "Haifa";
-                Pet pet = new Pet(name, petAge, kind, sex, location, number, contact_name);
                 try{
-                    DBWrapper.addNewPet(pet, user, imagePathList, AddPetActivity.this);
+                    DBWrapper.addNewPet(CollectPetInfo(), user, imagePathList, AddPetActivity.this);
                 }catch(Exception e){
                     Log.e(TAG, "Error Adding pet to DB! " + e.getMessage());
                 }
@@ -174,17 +164,23 @@ public class AddPetActivity extends AppCompatActivity {
         };
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         Log.i(TAG, "In onActivityResult");
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
                 && data != null)
         {
             imagePathList = new ArrayList<>();
-            if(data.getClipData() != null){
+            if(data.getClipData() != null)
+            {
                 Log.i(TAG, "Get pictures selected by user.");
-                int count = data.getClipData().getItemCount();
-                for (int i=0; i<count; i++){
+                int numOfPics = data.getClipData().getItemCount();
+                if(numOfPics>MAX_NUMBER_OF_IMAGES)
+                {
+                    Toast.makeText(getApplication(), "only 3 pictures are allowed", Toast.LENGTH_SHORT).show();
+                }
+                for (int i=0; i<numOfPics && i < MAX_NUMBER_OF_IMAGES; i++){
                     try {
                         Uri imageUri = data.getClipData().getItemAt(i).getUri();
                         imagePathList.add(imageUri);
@@ -194,7 +190,8 @@ public class AddPetActivity extends AppCompatActivity {
                     }
                 }
             }
-            else if(data.getData() != null){
+            else if(data.getData() != null)
+            {
                 Log.i(TAG, "Get one picture selected by user.");
                 try{
                     Uri imgUri = data.getData();
@@ -207,6 +204,29 @@ public class AddPetActivity extends AppCompatActivity {
             }
         }
         else Log.i(TAG, "No pictures selected by user.");
+
+        this.RefreshPicturesView();// this will show the image
+
         }
+    protected void RefreshPicturesView()
+    {
+        Uri uri;
+        for (int i=0;i<MAX_NUMBER_OF_IMAGES;i++)
+        {
+            if(i<imagePathList.size())
+            {
+                uri = imagePathList.get(i);
+                DBWrapper.imageViewLoadUri(picturesView[i],uri);
+            }
+            else
+            {
+                picturesView[i].setImageResource(android.R.color.transparent);
+            }
+
+        }
+    }
 
 }
+
+
+
